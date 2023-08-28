d90469811efd0cba8d3cfdcf48f6841cb33f018c
ajith28 <ajith@sketchbrahma.com>
2023-08-28 19:02:14 +0530

    header and content component created

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <div className="logo">logo</div>
+        <div className="menu">
+        <div className="menu-item">About</div>
+        <div className="menu-item">Journal</div>
+        <div className="menu-item">Contact</div>
+      </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Header;
