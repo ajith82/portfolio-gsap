810aebd1addd2f383daab52960c55ff0c10c88b3
ajith28 <ajith@sketchbrahma.com>
2023-08-30 10:33:02 +0530

    content component created

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./content.css";
 
 const Content = () => {
-  return <div>Content</div>;
+  return (
+    <div className="content">
+      <h1 className="content-inner-bold">
+        <div>Hard work beats talent</div>
+        <br />
+        <div>when tallent doesn't work hard.</div>
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at
+        feugiat felis. In id blandit arcu. Vestibulum urna sapien, molestie ut
+        pulvinar ultricies, bibendum ut magna. Cras ac tincidunt nulla.
+      </p>
+      <button>Explore</button>
+    </div>
+  );
 };
 
 export default Content;
